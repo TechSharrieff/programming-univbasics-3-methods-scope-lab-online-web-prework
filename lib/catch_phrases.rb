99cca2ade82be12d682dd6e8d31f84b36edc3ca4
Sharrieff ross <crross@dons.usfca.edu>
2019-08-10 04:10:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,7 @@ def toadstool
   puts status
 end
 
+def link
+  it "puts out 'It's Dangerous To Go Alone! Take This.'" do
+    expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
+  end
