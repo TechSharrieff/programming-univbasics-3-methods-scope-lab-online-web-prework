b5d1a368b39f93aa1528a041358c37448c025dca
Sharrieff ross <crross@dons.usfca.edu>
2019-08-10 04:41:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,3 +13,11 @@ def link
   puts fast
 end
     
+def any_phrase(phrase)
+  
+  
+  it "takes in an argument and puts out the catch phrase" do
+    phrase = "Do A Barrel Roll!"
+    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+  end
+end
